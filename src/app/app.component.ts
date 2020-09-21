00e94d88f2e78f6c585c41219e9c7db728376393
fibogroup <jhonarizafibo@gmail.com>
2020-09-21 20:58:58 +1000

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'scap-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
